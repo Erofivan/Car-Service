ce6bab0ef1a58c96ce51fcc50ace1584ed0d7f10
ivanerofeev <ivanerofeev01@gmail.com>
2026-04-08 18:28:18 +0300

    feat: remove ids

--- src/main/java/com/erofivan/presentation/dtos/requests/PlaceInventoryOrderRequest.java
+++ src/main/java/com/erofivan/presentation/dtos/requests/PlaceInventoryOrderRequest.java
@@ -3,7 +3,6 @@ package com.erofivan.presentation.dtos.requests;
 import java.util.UUID;
 
 public record PlaceInventoryOrderRequest(
-    UUID clientId,
     UUID carId
 ) {
 }
--- src/main/java/com/erofivan/presentation/dtos/requests/ScheduleTestDriveRequest.java
+++ src/main/java/com/erofivan/presentation/dtos/requests/ScheduleTestDriveRequest.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record ScheduleTestDriveRequest(
-    UUID clientId,
     UUID carId,
     LocalDateTime startsAt
 ) {
