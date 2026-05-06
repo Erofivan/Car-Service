ba4008002eefdfd73b26839169f3a3584798ae49
ivanerofeev <ivanerofeev01@gmail.com>
2026-05-06 13:49:56 +0300

    feat: add graalVM optl compatability

--- order-service/src/main/java/com/erofivan/App.java
+++ order-service/src/main/java/com/erofivan/App.java
@@ -2,8 +2,21 @@ package com.erofivan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
+
+import com.erofivan.presentation.dtos.responses.*;
+import com.erofivan.presentation.dtos.requests.*;
 
 @SpringBootApplication
+@RegisterReflectionForBinding({
+    CarAvailabilityResponse.class,
+    InventoryOrderResponse.class,
+    TestDriveResponse.class,
+    CustomOrderResponse.class,
+    PlaceCustomOrderRequest.class,
+    ScheduleTestDriveRequest.class,
+    PlaceInventoryOrderRequest.class
+})
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
--- storage-service/src/main/java/com/erofivan/App.java
+++ storage-service/src/main/java/com/erofivan/App.java
@@ -2,8 +2,27 @@ package com.erofivan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
+
+import com.erofivan.presentation.dtos.*;
+import com.erofivan.presentation.dtos.responses.*;
+import com.erofivan.presentation.dtos.requests.*;
+import com.erofivan.contracts.events.*;
 
 @SpringBootApplication
+@RegisterReflectionForBinding({
+    ConfigurationOption.class,
+    CarAvailabilityResponse.class,
+    AssemblyOrderResponse.class,
+    ConfigurationResponse.class,
+    PartResponse.class,
+    CarResponse.class,
+    CreateAssemblyOrderRequest.class,
+    BuildConfigurationRequest.class,
+    CustomOrderWarehouseApprovedEvent.class,
+    InventoryOrderCancelledEvent.class,
+    InventoryOrderPlacedEvent.class
+})
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
