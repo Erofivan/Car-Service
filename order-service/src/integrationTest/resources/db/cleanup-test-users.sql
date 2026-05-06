872265df57c46acb663edb5beaa85ea5f399d772
ivanerofeev <ivanerofeev01@gmail.com>
2026-05-06 16:09:17 +0300

    fix: semicolon missing syntax error

--- order-service/src/integrationTest/resources/db/cleanup-test-users.sql
+++ order-service/src/integrationTest/resources/db/cleanup-test-users.sql
@@ -2,7 +2,7 @@ DELETE FROM test_drive_requests;
 DELETE FROM inventory_orders;
 DELETE FROM custom_orders;
 DELETE FROM outbox_events;
-DELETE FROM users;
+DELETE FROM users
 WHERE id IN (
              '50000000-0000-0000-0000-000000000001',
              '50000000-0000-0000-0000-000000000002'
